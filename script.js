cee8592243fe915d750ae7cfb5e12f7ab1d7f6ff
enterkvas <alexanderbugakov66@gmail.com>
2022-02-16 12:18:15 +0400

    Got modal window

--- script.js
+++ script.js
@@ -2,10 +2,25 @@ const modal = new tingle.modal({
     footer: false,
     stickyFooter: false,
     closeMethods: ['overlay', 'button', 'escape'],
-    closeLabel: "Закрыть"
-  })
+    closeLabel: "Закрыть",
 
-  const popupOpen = (link) => {
-      modal.open()
-      modal.setContent(document.querySelector(`#${link}`))
-  }
+})
+
+const getButtons = document.querySelectorAll('.button');
+for(let i = 0; i < getButtons.length; i++) {
+    const getButton = getButtons[i];
+    getButton.addEventListener("click", function() {
+        modal.open()
+        modal.setContent(document.querySelector(`.hidden-container`))
+    })        
+}
+
+"use strict";
+
+// Кнопка бургера /Показ\скрытие /Смена цвета плашки /Прокрутка к началу страницы 
+const iconBurger = document.querySelector('.burger-button');
+const navList = document.querySelector('.header-menu');
+iconBurger.addEventListener("click", function(e) {   
+  navList.classList.toggle('_active'); // Меню выезжает/уезжает (toggle - добавляет/удаляет класс)
+  document.body.classList.toggle('_lock'); // Предотвращаем скролл страницы под выехавшим меню бургера      
+});
