d654bde866198af4a253c385d0df3f687e651e5b
enterkvas <alexanderbugakov66@gmail.com>
2022-02-20 21:08:02 +0400

    The last commit

--- script.js
+++ script.js
@@ -17,10 +17,34 @@ for(let i = 0; i < getButtons.length; i++) {
 
 "use strict";
 
-// Кнопка бургера /Показ\скрытие /Смена цвета плашки /Прокрутка к началу страницы 
+// Кнопка бургера Показ\скрытие 
 const iconBurger = document.querySelector('.burger-button');
 const navList = document.querySelector('.header-menu');
 iconBurger.addEventListener("click", function(e) {   
   navList.classList.toggle('_active'); // Меню выезжает/уезжает (toggle - добавляет/удаляет класс)
   document.body.classList.toggle('_lock'); // Предотвращаем скролл страницы под выехавшим меню бургера      
-});
+})
+
+// Логика по клику на кнопку Заказать
+// ----------------------------------------
+// Получение текстов из элементов h3
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.forms.main;
+    let inputNew = document.createElement('input');
+    form.appendChild(inputNew);
+    inputNew.name = 'newInput';
+    inputNew.value = 'услуга';
+    inputNew.type = 'text';
+    let mainLinks = document.querySelectorAll('.main-link');
+    for (let i = 0; i < mainLinks.length; i++) {
+        const mainLink = mainLinks[i];    
+        let textLanding = mainLinks[0].innerText;
+        let textShop = mainLinks[1].innerText;
+        let textBoard = mainLinks[2].innerText;        
+        console.log(textBoard);      
+    }
+    mainButtonLanding = document.querySelector('.buttonLanding');
+    mainButtonLanding.addEventListener("click", function() {
+
+    })
+})
